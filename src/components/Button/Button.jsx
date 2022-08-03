299b51a1c2d63bda5b54b348dcb44157dd6fe5b7
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-08-03 23:30:37 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,16 @@
-import { Component } from 'react';
 import css from './Button.module.css';
 import PropTypes from 'prop-types';
-export class Button extends Component {
-  onClick = () => {
-    this.props.onClick(1);
+export const Button = ({ onClick }) => {
+  const onBtnClick = () => {
+    onClick(1);
   };
-  render() {
-    return (
-      <button type="button" className={css.button} onClick={this.onClick}>
-        Load more
-      </button>
-    );
-  }
-}
+
+  return (
+    <button type="button" className={css.button} onClick={onBtnClick}>
+      Load more
+    </button>
+  );
+};
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
