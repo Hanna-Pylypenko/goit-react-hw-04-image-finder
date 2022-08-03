60882358a6363fdd1baa34258edae2f8407e364d
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-08-03 23:22:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { SearchBar } from './SearchBar/SearchBar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class App extends Component {
-  state = {
-    searchedItem: '',
+export const App = () => {
+  const [searchedItem, setSearchedItem] = useState('');
+
+  const onSubmit = data => {
+    setSearchedItem(data);
   };
 
-  onSubmit = data => {
-    this.setState({
-      searchedItem: data,
-    });
-  };
+  return (
+    <div
+      style={{
+        margin: 0,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <SearchBar onSubmit={onSubmit} />
 
-  render() {
-    return (
-      <div
-        style={{
-          margin: 0,
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <SearchBar onSubmit={this.onSubmit} />
-
-        <ToastContainer className="toastContainer" />
-        <ImageGallery searchedItem={this.state.searchedItem} />
-      </div>
-    );
-  }
-}
+      <ToastContainer className="toastContainer" />
+      <ImageGallery searchedItem={searchedItem} />
+    </div>
+  );
+};
