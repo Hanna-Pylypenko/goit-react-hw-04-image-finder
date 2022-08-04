010d8ca4f70827a0e352d4b3d86cb737e4126b3a
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-08-04 21:23:35 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,28 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import css from './Modal.module.css';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
+export const Modal = ({ toggleModal, modalImage, alt }) => {
+  useEffect(() => {
     window.addEventListener('keydown', e => {
       if (e.code === 'Escape') {
-        this.props.toggleModal();
+        toggleModal();
       }
     });
-  }
-  render() {
-    return createPortal(
-      <div className={css.overlay} onClick={this.props.toggleModal}>
-        <div className={css.modal}>
-          <img src={this.props.modalImage} alt={this.props.alt} width="700" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  });
+
+  return createPortal(
+    <div className={css.overlay} onClick={toggleModal}>
+      <div className={css.modal}>
+        <img src={modalImage} alt={alt} width="700" />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 Modal.propTypes = {
   toggleModal: PropTypes.func.isRequired,
   modalImage: PropTypes.string.isRequired,
