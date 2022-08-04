c74eee23c32d58230f193940ab1b8dda8bb6867e
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-08-04 21:18:38 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,36 +1,30 @@
 import css from './ImageGalleryItem.module.css';
 import { Modal } from 'components/Modal/Modal';
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    modalOpen: 'closed',
-  };
-  toggleModal = () => {
-    if (this.state.modalOpen === 'closed') {
-      this.setState({ modalOpen: 'opened' });
+export const ImageGalleryItem = ({ src, alt, modalImage }) => {
+  const [modalOpen, setModalState] = useState('closed');
+
+  const toggleModal = () => {
+    if (modalOpen === 'closed') {
+      setModalState('opened');
     }
-    if (this.state.modalOpen === 'opened') {
-      this.setState({ modalOpen: 'closed' });
+    if (modalOpen === 'opened') {
+      setModalState('closed');
     }
   };
-  render() {
-    const { alt, src, modalImage } = this.props;
-    return (
-      <li className={css.galleryItem} onClick={this.toggleModal}>
-        <img src={src} alt={alt} />
-        {this.state.modalOpen === 'opened' && (
-          <Modal
-            toggleModal={this.toggleModal}
-            modalImage={modalImage}
-            alt={alt}
-          />
-        )}
-      </li>
-    );
-  }
-}
+
+  return (
+    <li className={css.galleryItem} onClick={toggleModal}>
+      <img src={src} alt={alt} />
+      {modalOpen === 'opened' && (
+        <Modal toggleModal={toggleModal} modalImage={modalImage} alt={alt} />
+      )}
+    </li>
+  );
+};
+
 ImageGalleryItem.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
